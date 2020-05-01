74a4b8b614692e184b8b3f5361a29d8806e93d60
mishamin116@gmail.com <mishamin116@gmail.com>
2020-05-01 22:16:00 +0700

    welcome phrase

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.json({ extended: true }));
+app.use(express.json({ extended: true }));  
 
 app.use("/api/forge", require("./routes/forge.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/users.routes"));
 
 app.get("*", (req, res) => {  
-  res.send("Server is running");
+  res.send(`BIMgen Server is running. Letq's hack...`);
 });
 
 const PORT = config.get('port') || 5000
