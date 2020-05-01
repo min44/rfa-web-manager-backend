df6ce644d10db4421cf4017b239add4d2d8e4178
mishamin116@gmail.com <mishamin116@gmail.com>
2020-05-01 20:05:17 +0700

    port and config

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
 
 
 if (process.env.NODE_ENV === "production") {
-  PORT = process.env.PORT || 3000;
+  PORT = process.env.PORT || 80;
   // app.use("/", express.static(path.join(__dirname, "client", "build")));
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
