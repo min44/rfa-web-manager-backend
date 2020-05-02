b3a9d9200736ef6cfdb3d4e99dbb0ea07c754225
mishamin116@gmail.com <mishamin116@gmail.com>
2020-05-02 15:28:07 +0700

    production setup_1

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
+const cors = require("cors");
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 
 const app = express();
 
-app.use(express.json({ extended: true }));  
+app.use(express.json({ extended: true }));
 app.use(cors());
 
 app.use("/api/forge", require("./routes/forge.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/users.routes"));
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {
   res.send(`BIMgen Server is running. Lets hack...`);
 });
 
-const PORT = config.get('port') || 5000
+const PORT = config.get("port") || 5000;
+const baseURL = config.get("baseURL");
 
-// serve the API with signed certificate on 443 (SSL/HTTPS) port
-const httpsServer = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/api.bimgen.org/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/api.bimgen.org/fullchain.pem'),
-}, app);
-httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
-});
+if (process.env.NODE_ENV === "production") {
+  // serve the API with signed certificate on 443 (SSL/HTTPS) port
+  const httpsServer = https.createServer(
+    {
+      key: fs.readFileSync(`/etc/letsencrypt/live/${baseURL}/privkey.pem`),
+      cert: fs.readFileSync(`/etc/letsencrypt/live/${baseURL}/fullchain.pem`),
+    },
+    app
+  );
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
+  // serve the API on 80 (HTTP) port
+  const httpServer = http.createServer(app);
+  httpServer.listen(80, () => {
+    console.log("HTTP Server running on port 80");
+  });
+}
 
-// serve the API on 80 (HTTP) port
-const httpServer = http.createServer(app);
-httpServer.listen(80, () => {
-    console.log('HTTP Server running on port 80');
-});
+console.log(process.env.NODE_ENV)
 
 async function start() {
   try {
