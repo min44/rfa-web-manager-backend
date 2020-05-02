c33172a1a68ed655c14523e4778d21ea08ce2efc
mishamin116@gmail.com <mishamin116@gmail.com>
2020-05-02 13:42:46 +0700

    https

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const cors = require('cors');
+const https = require('https');
+const http = require('http');
 
 const app = express();
 
 app.use(express.json({ extended: true }));  
+app.use(cors());
 
 app.use("/api/forge", require("./routes/forge.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/users.routes"));
-
-app.get("*", (req, res) => {  
+app.get("/", (req, res) => {  
   res.send(`BIMgen Server is running. Lets hack...`);
 });
 
 const PORT = config.get('port') || 5000
+
+// serve the API with signed certificate on 443 (SSL/HTTPS) port
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/my_api_url/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
+}, app);
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
+
+// serve the API on 80 (HTTP) port
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
 
 async function start() {
   try {
